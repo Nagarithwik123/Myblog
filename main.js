34bc82d093a9a9fe5bb66fdb47e91711b857cb64
Nagarithwik123 <81247077+Nagarithwik123@users.noreply.github.com>
2021-05-20 16:58:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,40 +1,29 @@
-canvas=document.getElementById("myCanvas");
-ctx=canvas.getContext("2d");
-color="blue";
-
-ctx.beginPath();
-ctx.strokeStyle=color;
-ctx.lineWidth=1;
-ctx.rect(150,143,430,200);
-ctx.stroke();
-
-ctx.beginPath();
-ctx.strokeStyle=color;
-ctx.lineWidth=1;
-ctx.arc(250,210,40,0,2*Math.PI);
-ctx.stroke();
-
-ctx=canvas.getContext("2d");
-color="blue";
-
-canvas.addEventListener("mousedown",my_mousedown);
-
-function my_mousedown(e){
-    color=document.getElementById("color").value;
-    console.log(color);
-
-    mouse_x=e.clientX - canvas.offsetLeft;
-    mouse_y=e.clientY - canvas.offsetTop;
-    console.log("X="+ mouse_x+",Y="+mouse_y);
-    circle(mouse_x,mouse_y);
+var item=["paneer tandoori pizza","margherita pizza","veg supreme pizza","spicy veg pizza","corn and onion pizza"];
+function getmenu(){
+    var htmldata;
+    htmldata="<ol class='menulist'>"
+    item.sort();
+for(var i=0;i<item.length;i++){
+    htmldata=htmldata+'<li>'+ item[i]+ '</li>'
 }
-function circle(mouse_x,mouse_y){
-    ctx.beginPath();
-ctx.strokeStyle=color;
-ctx.lineWidth=2;
-ctx.arc(mouse_x,mouse_y,40,0,2*Math.PI);
-ctx.stroke();
+htmldata=htmldata+"</ol>"
+document.getElementById("menu_list").innerHTML=htmldata;
 }
-  function clearArea(){
-      ctx.clearRect(0,0,canvas.width,canvas.height);
-  }
+function add_top(){
+    var htmldata;
+    var item2= document.getElementById("add_item").value;
+    item.push(item2);
+    item.sort();
+    htmldata="<section class='cards'>"
+    for(var i=0;i<item.length;i++){
+        htmldata=htmldata+'<div class="card">' + '<img src="pizza.png"/>' + item[i] + '</div>';
+    }
+    htmldata=htmldata+"Add";
+    document.getElementById("add_toppings").innerHTML=htmldata;
+}
+function add_item(){
+    var item2=document.getElementById("add_top").value;
+    item.push(item2);
+    add_top();
+}
+    
