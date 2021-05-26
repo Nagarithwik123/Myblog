626060a9728e060be0382d5b7dcaab61b77e1a4c
Nagarithwik123 <81247077+Nagarithwik123@users.noreply.github.com>
2021-05-26 16:52:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,98 +1,46 @@
-canvas=document.getElementById("myCanvas");
-ctx=canvas.getContext("2d");
+var names_of_people = [];
+    
+function submit()
+{
+    var GuestName = document.getElementById("name1").value;
+	names_of_people.push(GuestName);
 
-car1_width=100;
-car1_height=90;
-car1_img="car1.png";
-car1_x=10;
-car1_y=10;
-
-car2_width=100;
-car2_height=90;
-car2_img="car2.png";
-car2_x=10;
-car2_y=100;
-
-background_image="racing.jpg";
-
-function add() {
-    background_imgtag=new Image();
-    background_imgtag.onload=uploadbackground;
-    background_imgtag.src=background_image;
-
-    car1_imgtag=new Image();
-    car1_imgtag.onload=uploadcar1;
-    car1_imgtag.src=car1_img;
-
-    car2_imgtag=new Image();
-    car2_imgtag.onload=uploadcar2;
-    car2_imgtag.src=car2_img;
-}
-
-function uploadbackground() {
-    ctx.drawImage(background_imgtag, 0, 0, canvas.width, canvas.height);
-}
-
-function uploadcar1() {
-    ctx.drawImage(car1_imgtag, car1_x, car1_y, car1_width, car1_height);
-}
-
-function uploadcar2() {
-    ctx.drawImage(car2_imgtag, car2_x, car2_y, car2_width, car2_height);
-}
-
-window.addEventListener("keydown",my_keydown);
-
-function my_keydown(e) {
-    keypressed=e.keyCode;
-    console.log(keypressed);
-
-    if (keypressed=="38") {
-        car1_up();
-        console.log("up");
-    }
-
-    if (keypressed=="40") {
-        car1_down();
-        console.log("down");
-    }
-
-    if (keypressed=="37") {
-        car1_left();
-        console.log("left");
-    }
-
-    if (keypressed=="39") {
-        car1_right();
-        console.log("right");
-    }
-
-    if (keypressed=='87'){
-        car2_up();
-        console.log("up W");
-    }
-
-    if (keypressed=='65'){
-        car2_left();
-        console.log("left A");
-    }
+	console.log(GuestName);
         
-    if (keypressed=='83'){
-        car2_down();
-        console.log("down S");
-    }
-    if (keypressed=='68'){
-        car2_right();
-        console.log("right D");
-    }
-    if (car1_x > 700) {
-        console.log("car1 won");
-        document.getElementById('game_status').innerHTML = "Car 1 Won!!!!!!!!!!!!!"
-    }
+    console.log(names_of_people);
+    var lenght_of_name = names_of_people.length;
+    console.log(lenght_of_name);
+	document.getElementById("display_name").innerHTML=names_of_people.toString();
+	
+   }
 
-    if (car2_x > 700) {
-        console.log("car2 won");
-        document.getElementById('game_status').innerHTML = "Car 2 Won!!!!!!!!!!!!!"
-    }
+function sorting()
+{
+	names_of_people.sort();
+	var i= names_of_people.join("<br>");
+    console.log(names_of_people);		
+	document.getElementById("sorted").innerHTML=i.toString();
+	}
+
+function show()
+{
+	var i= names_of_people.join("<br>");
+	console.log(names_of_people);
+	document.getElementById("p1").innerHTML=i.toString();
+	document.getElementById("sort_button").style.display="block";
+	}
+
+function searching()
+{
+	var s= document.getElementById("s1").value;
+	var found=0;
+	var j;
+	for(j=0; j<names_of_people.length; j++)
+		{
+			if(s==names_of_people[j]){
+				found=found+1;
+			}	
+		}
+	document.getElementById("p2").innerHTML="name found "+found+" time/s";
+	console.log("found name "+found+" time/s");
 }
-
