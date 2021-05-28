bf3bf5cd2dc91e60e9c90194ec32525063bc66dc
Nagarithwik123 <81247077+Nagarithwik123@users.noreply.github.com>
2021-05-28 18:03:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,8 +13,8 @@ function player_update(){
     fabric.Image.fromURL("player.png",function(Img){
         player_object=Img;
 
-        player_object.scaleToWidth(150);
-        player_object.scaleToHeight(150);
+        player_object.scaleToWidth(block_image_width);
+        player_object.scaleToHeight(block_image_height);
         player_object.set({
             top:player_y,
             left:player_x
@@ -65,7 +65,7 @@ function my_keydown(e)
         left();
         console.log("left");
     }
-    if(keyPressed=='38'){
+    if(keyPressed=='39'){
         right();
         console.log("right");
         }
@@ -88,5 +88,41 @@ function my_keydown(e)
     if(keyPressed=='72'){
         new_image('captain_america_left_hand.png');
         console.log("r");
+    }
+}
+function up(){
+      if(player_y>=0){
+          player_y-=block_image_height;
+          console.log("block_image_height="+block_image_height);
+          console.log("when Up arrow is prssed,x="+player_x + "y=" +player_y);
+          canvas.remove(player_object);
+            player_update();
+      }
+  }
+  function down(){
+    if(player_y<=500){
+        player_y+=block_image_height;
+        console.log("block_image_height="+block_image_height);
+        console.log("when Down arrow is prssed,x="+player_x + "y=" +player_y);
+        canvas.remove(player_object);
+          player_update();
+    }
+}
+function left(){
+    if(player_x>=0){
+        player_x-=block_image_width;
+        console.log("block_image_width="+block_image_width);
+        console.log("when Left arrow is prssed,x="+player_x + "y=" +player_y);
+        canvas.remove(player_object);
+          player_update();
+    }
+}
+function right(){
+    if(player_x<=850){
+        player_x+=block_image_width;
+        console.log("block_image_width="+block_image_width);
+        console.log("when Right arrow is prssed,x="+player_x + "y=" +player_y);
+        canvas.remove(player_object);
+          player_update();
     }
 }
