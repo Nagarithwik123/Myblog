ab4e39bf97e9a29eef3a48d41ee6024183fa32d9
Nagarithwik123 <81247077+Nagarithwik123@users.noreply.github.com>
2021-06-24 18:46:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function take_snapshot(){
 }
 
 console.log('ml5 version:',ml5.version);
-classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JS4Hlu16M/model.json',modelLoaded);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/8QpQep7-7/model.json',modelLoaded);
 
 function modelLoaded(){
     console.log('model loaded');
@@ -32,7 +32,7 @@ function gotResult(error,results){
     }
     else{
         console.log(results);
-        document.getElementById("result_object_name").innerHTML=results[0].label;
-        document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+        document.getElementById("result_member_name").innerHTML=results[0].label;
+        document.getElementById("result_member_accuracy").innerHTML=results[0].confidence.toFixed(3);
     }
 }
