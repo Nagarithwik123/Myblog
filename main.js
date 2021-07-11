f45af9ca692b01364d746705deb06f29129887c1
Nagarithwik123 <81247077+Nagarithwik123@users.noreply.github.com>
2021-07-11 20:49:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,83 +1,28 @@
-prediction1=""
-prediction2=""
+function setup() {
+    canvas = createCanvas(640,480);
+    canvas.position(20, 150);
+    video = createCapture(VIDEO);
+    video.hide();
+}
 
-Webcam.set({
-    width:350,
-    height:300,
-    image_format:'png',
-    png_quality:90
-});
+function draw() {
+    image(video, 20, 40, 600, 400);
 
-camera=document.getElementById("camera");
+    fill(255, 255, 0);
+    stroke(0, 0, 0);
+    circle(20, 50, 40);
+    circle(620, 50, 40);
+    circle(20, 430, 40);
+    circle(620, 430, 40);
 
-Webcam.attach ('#camera');
+    fill(255,165,0);
+    stroke(0, 0, 0);
+    rect(40, 30, 560, 20);
+    rect(10, 70, 20, 340);
+    rect(40, 430, 560, 20);
+    rect(610, 70, 20, 340);
+}
 
 function take_snapshot(){
-    Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
-    });
-}
-
-console.log('ml5 version:',ml5.version);
-classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/gOfRLEBDF/model.json',modelLoaded);
-
-function modelLoaded(){
-    console.log('Model Loaded!');
-}
-
-function speak(){
-    var synth = window.speechSynthesis;
-    speak_data_1="The First Prediction is"+prediction1;
-    speak_data_2="The Second Prediction is"+prediction2;
-    var utterThis = new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
-    synth.speak(utterThis);
-}
-
-function check(){
-    img = document.getElementById('captured_image');
-    classifier.classify(img,gotResult);
-}
-
-function gotResult(error,results){
-    if(error){
-        console.error(error);
-    }
-    else{
-        console.log(results);
-        document.getElementById("result_object_name").innerHTML = results[0].label;
-        document.getElementById("result_object_name_two").innerHTML = results[1].label;
-        prediction1=results[0].label;
-        prediction2=results[1].label;
-        speak();
-        if(results[0].label == "Awesome"){
-            document.getElementById("result_object_gesture_icon").innerHTML = "&#128076;";
-        }
-        if(results[0].label == "All the Best"){
-            document.getElementById("result_object_gesture_icon").innerHTML = "&#128077;";
-        }
-        if(results[0].label == "Two"){
-            document.getElementById("result_object_gesture_icon").innerHTML = "&#9996;";
-        }
-        if(results[0].label == "Punch"){
-            document.getElementById("result_object_gesture_icon").innerHTML = "&#9994;";
-        }
-        if(results[0].label == "Yo-Yo"){
-            document.getElementById("result_object_gesture_icon").innerHTML = "&#129304;";
-        }
-        if(results[1].label == "Awesome"){
-            document.getElementById("result_object_gesture_icon2").innerHTML = "&#128076;";
-        }
-        if(results[1].label == "All the Best"){
-            document.getElementById("result_object_gesture_icon2").innerHTML = "&#128077;";
-        }
-        if(results[1].label == "Two"){
-            document.getElementById("result_object_gesture_icon2").innerHTML = "&#9996;";
-        }
-        if(results[1].label == "Punch"){
-            document.getElementById("result_object_gesture_icon2").innerHTML = "&#9994;";
-        }
-        if(results[1].label == "Yo-Yo"){
-            document.getElementById("result_object_gesture_icon2").innerHTML = "&#129304;";
-        }
-    }
+    save('birthday_snapshot.png');
 }
