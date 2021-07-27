6529338c294e2f7d6f2a14a23eb5954555a053a6
Nagarithwik123 <81247077+Nagarithwik123@users.noreply.github.com>
2021-07-27 18:37:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
-function setup(){
-    canvas = createCanvas(270,270);
-    canvas.center();
-    video = createCapture(VIDEO);
-    video.hide();
-    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/8QpQep7-7/model.json',modelLoaded);
+function setup() {
+  canvas = createCanvas(280, 280);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = ml5.imageClassifier('mobilenet',modelLoaded);
 }
-
 function draw(){
- image(video,0,0,270,270);
- classifier.classify(video,gotResult);
+  image(video,0,0,280,280);
+  classifier.classify(video,gotResult);
 }
-
 function modelLoaded(){
-    console.log("model is loaded");
+  console.log('model is loaded');
+}
+function gotResult(error,results){
+  if(error){
+    console.error(error);
+  }
+  else{
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML= results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML= results[0].confidence.toFixed(3);
+  }
 }
 
-function gotResult(error,results){
-    if(error){
-        console.error(error);
-    }
-    else{
-        console.log(results);
-        document.getElementById("result_family_member_name").innerHTML=results[0].label;
-        document.getElementById("result_family_member_accuracy").innerHTML=results[0].confidence.toFixed(3);
-    }
-}
+
+
