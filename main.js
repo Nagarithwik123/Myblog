592290a9bf4fc836f003ff4e74bd5af8fe6f2ec7
Nagarithwik123 <81247077+Nagarithwik123@users.noreply.github.com>
2021-08-07 18:19:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,50 +1,38 @@
-var SpeechRecognition = window.webkitSpeechRecognition;
-var recognition = new SpeechRecognition();
-
-function start(){
-    document.getElementById("text_box").innerHTML = "";
-    recognition.start();
-}
-
-recognition.onresult= function(event){
-    console.log(event);
-    var Content=event.results[0][0].transcript;
-    document.getElementById("text_box").innerHTML=Content;
-    console.log(Content);
-    if(Content == "take my selfie"){
-        console.log("taking selfie ------");
-        speak();
-    }   
-}
-
-function speak(){
-    var synth = window.speechSynthesis;
-    speak_data = "taking your selfie in 5 seconds";
-    var utterThis = new SpeechSynthesisUtterance(speak_data);
-    synth.speak(utterThis);
-    Webcam.attach(camera);
-    setTimeout(function(){
-        take_snapshot();
-        save();
-    },5000);
-}
-
 Webcam.set({
-    width:360,
-    height:250,
-    image_format:'jpeg',
-    jpeg_quality:90
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
 });
-camera=document.getElementById("camera");
+
+camera =document.getElementById("camera");
+Webcam.attach('#camera');
 
 function take_snapshot(){
     Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML='<img id="selfie_image" src="'+data_uri+'"/>';
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
     });
 }
-function save(){
-    link = document.getElementById("link");
-    image = document.getElementById("selfie_image").src;
-    link.href = image;
-    link.click();
+
+console.log('ml5 version:',ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JS4Hlu16M/model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log('model loaded');
+}
+
+function check(){
+    img=document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML=results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
 }
